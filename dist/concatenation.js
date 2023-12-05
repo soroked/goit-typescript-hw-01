56c043e12cacf214bc1c18f60eb53599c02c5519
Eduard <sorokolietov@gmail.com>
2023-12-05 10:48:33 +0100

    init

---
+++ dist/concatenation.js
@@ -0,0 +1,7 @@
+function concatenation(firstWord, secondWord) {
+    console.log('before Breakpoint');
+    console.log(`${firstWord} ${secondWord}`);
+    console.log('after Breakpoint');
+}
+export { concatenation };
+//# sourceMappingURL=concatenation.js.map
---
+++ src/concatenation.ts
@@ -0,0 +1,7 @@
+function concatenation(firstWord: string, secondWord: string) {
+  console.log('before Breakpoint');
+  console.log(`${firstWord} ${secondWord}`);
+  console.log('after Breakpoint');
+}
+
+export { concatenation };
